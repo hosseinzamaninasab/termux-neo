7ae55fafac952db6a44fac59b510639ec6f891ba
Roronoa Zoro <zoro@local>
2026-07-16 20:31:07 +0330

    Refactor: introduce dynamic UI width

---
+++ tests/test_ui.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+source src/utils.sh
+
+ui_init
+ui_title "TERMUX NEO"
+
+ui_add_row "USER" "Zoro"
+ui_add_row "DEVICE" "Samsung Note5"
+
+ui_add_status "Wi-Fi"
+ui_add_status "VPN"
+
+ui_render
